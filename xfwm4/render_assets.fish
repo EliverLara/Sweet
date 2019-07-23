0bc76e881b7e7b0f61708e0084ec379c9806e656
EliverLara <eliverlara@gmail.com>
2019-07-23 11:05:18 -0500

    Fix resizing borders not showing in xfce desktop

--- xfwm4/render_assets.fish
+++ xfwm4/render_assets.fish
@@ -1,4 +1,12 @@
+set -l red (set_color -o red)
+set -l cyan (set_color -o cyan)
+
 for i in assets/*;
 	set -l file_name (basename $i .svg)
-	convert -background none $i ./$file_name'.png'
+	if test -f ./$file_name'.png'
+		echo $red$file_name exists
+	else
+		echo $cyan'Creating '$file_name'.png'
+		convert -background none $i ./$file_name'.png'
+	end
 ;end
